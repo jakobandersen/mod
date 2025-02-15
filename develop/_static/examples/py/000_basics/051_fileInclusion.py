54eb71aee0966d29bfa7c38ee36cf9a2a6e0c0ab
Jakob Lykke Andersen <Jakob@caput.dk>
2025-02-15 11:16:27 +0100

    Delete develop docs

--- develop/_static/examples/py/000_basics/051_fileInclusion.py
+++
@@ -1,9 +0,0 @@
-include("050_formoseGrammar.py")
-post.summarySection("Input Graphs")
-for a in inputGraphs:
-	a.print()
-post.summarySection("Input Rules")
-for a in inputRules:
-	a.print()
-# rst-name: Including Files
-# rst: We can include other files (a la C/C++) to separate functionality.
